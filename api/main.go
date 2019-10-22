df9e5dcae466791c8a19c4627431351e9551d10f
Yury Zhytkou <yury@iMac.local>
2019-10-22 19:05:11 -0400

    switch main to Lambda handler

--- api/main.go
+++ api/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
+	fmt.Printf("Body size = %d.\n", len(request.Body))
+
+	fmt.Println("Headers:")
+	for key, value := range request.Headers {
+		fmt.Printf("    %s: %s\n", key, value)
+	}
+
+	// process the request against the routes
+
+
+	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+}
+
 func main() {
 	exitIfError(loggerCreate())
 	exitIfError(versionPrint())
@@ -18,5 +41,10 @@ func main() {
 	exitIfError(viewsCleanupBegin())
 	exitIfError(ssoTokenCleanupBegin())
 
-	exitIfError(routesServe())
+	//exitIfError(routesServe()) //this is replaced by lambda request handler
+	// instead we need to just setup the routes
+	exitIfError(routesSetup())
+
+	// handle the request
+	lambda.Start(handleRequest)
 }
--- api/router.go
+++ api/router.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"net/http"
 	"os"
 )
 
-func routesServe() error {
+func routesSetup() error {
 	router := mux.NewRouter()
 
 	subdir := pathStrip(os.Getenv("ORIGIN"))
@@ -22,18 +20,6 @@ func routesServe() error {
 	if err := staticRouterInit(router); err != nil {
 		return err
 	}
-
-	origins := handlers.AllowedOrigins([]string{"*"})
-	headers := handlers.AllowedHeaders([]string{"X-Requested-With"})
-	methods := handlers.AllowedMethods([]string{"GET", "POST"})
-
-	addrPort := os.Getenv("BIND_ADDRESS") + ":" + os.Getenv("PORT")
-
-	logger.Infof("starting server on %s\n", addrPort)
-	if err := http.ListenAndServe(addrPort, handlers.CORS(origins, headers, methods)(router)); err != nil {
-		logger.Errorf("cannot start server: %v", err)
-		return err
-	}
-
+	
 	return nil
 }
